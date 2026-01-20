1b8a5c2a4e9bc57500da757f35bd4fb15a3a091d
Inside Out Energy Portal <energy-portal@insideout.nl>
2026-01-20 22:17:49 +0100

    Update Auth0 configuration with new Brouwer EMS application Client ID

--- src/auth0-config.ts
+++ src/auth0-config.ts
@@ -1,6 +1,6 @@
 export const auth0Config = {
   domain: "dev-17q5zgad7zxotyze.us.auth0.com",
-  clientId: "t1HvC0h3hFFssgrM2UraM8OS7BghcYSp",
+  clientId: "9jCW7uLStMV2oEuCUr3qhueJMUtqNlLI",
   authorizationParams: {
     redirect_uri: window.location.origin,
   },
